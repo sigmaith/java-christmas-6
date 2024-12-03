8abed3f618246aa9a0364dae9d8e9bcf1df5f227
Sumi Chung <drsoombre@naver.com>
2024-12-03 18:23:11 +0900

    style: 출력 형식 정돈

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,17 +8,20 @@ import java.text.NumberFormat;
 public class OutputView {
     public void printOrdersAndOriginalPrice(final Orders orders) {
         System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
         System.out.println("<주문 메뉴>");
         System.out.println(orders.toString());
         System.out.println();
         System.out.println("<할인 전 총주문 금액>");
-        System.out.println(orders.getWholePrices() + "원");
+        System.out.println(NumberFormat.getInstance().format(orders.getWholePrices()) + "원");
+        System.out.println();
     }
 
     public void printChampagnePromotion(final Menu champagne) {
+        System.out.println("<증정 메뉴>");
         if (champagne != null) {
             StringBuilder sb = new StringBuilder();
-            sb.append(champagne.getName()).append(" 1개");
+            sb.append(champagne.getName()).append(" 1개").append("\n");
             System.out.println(sb);
             return;
         }
@@ -31,22 +34,22 @@ public class OutputView {
 
     public void printGiftPromotion(final Menu champagne) {
         if (champagne != null) {
-            System.out.printf("증정 이벤트: -%s원", NumberFormat.getInstance().format(champagne.getPrice()));
+            System.out.printf("증정 이벤트: -%s원\n", NumberFormat.getInstance().format(champagne.getPrice()));
         }
     }
 
     public void printWeekDayOrWeekendPromotion(ApplicationInfo applicationInfo) {
         StringBuilder sb = new StringBuilder();
         if (applicationInfo.isWeekend()) {
-            System.out.printf("주말 할인: -%s원", NumberFormat.getInstance().format(applicationInfo.dcPrice()));
+            System.out.printf("주말 할인: -%s원\n", NumberFormat.getInstance().format(applicationInfo.dcPrice()));
             return;
         }
-        System.out.printf("평일 할인: -%s원", NumberFormat.getInstance().format(applicationInfo.dcPrice()));
+        System.out.printf("평일 할인: -%s원\n", NumberFormat.getInstance().format(applicationInfo.dcPrice()));
     }
 
     public void printChristmasDDayPromotinon(int dcPrice) {
         if (dcPrice != 0) {
-            System.out.printf("크리스마스 디데이 할인: -%s원", NumberFormat.getInstance().format(dcPrice));
+            System.out.printf("크리스마스 디데이 할인: -%s원\n", NumberFormat.getInstance().format(dcPrice));
         }
     }
 
